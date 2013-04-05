d74b466d4826a5a72708099151b9552d44b53751
Benji Decker <bensdecker@gmail.com>
2013-04-04 20:19:49 -0400

    Completed base to dos!

--- movie_server.rb
+++ movie_server.rb
@@ -1,9 +1,31 @@
 require 'sinatra'
+require 'pry'
+require 'rack-flash'
+require "sinatra"
+require "sinatra/reloader" 
 
 require_relative 'movies'
 
+enable :sessions
+use Rack::Flash
+
+before '/search_page' do
+  unless params[:password] == "coolbeans"
+    flash[:notice] = "Sorry, please enter correct password to access site!"
+    redirect '/landing'
+  end
+end
+
 get '/' do
- 	erb :index
+ 	erb :landing
+end
+
+get '/landing' do
+ 	erb :landing
+end
+
+post '/search_page' do
+ 	erb :search_page
 end
 
 post '/film_name' do
@@ -12,3 +34,19 @@ post '/film_name' do
 	m = Movie.get_film_info(name)
 	erb :movie, :locals => {:film => m}
 end
+
+
+
+# working on filn entry validation
+# post '/film_name' do
+# 	# could redirect /:film_name?
+# 	name = params[:film_name]
+# 	m = Movie.get_film_info(name)
+# 	unless m
+# 		erb :movie, :locals => {:film => m}
+# 	else
+# 		puts "doing this ish"
+# 		flash[:notice] = "Please enter a name!"
+# 		redirect '/'
+# 	end
+# end
